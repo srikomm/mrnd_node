d72fe522f901feeaef6f26fec2735165a8908ea1
srikomm <sriharsha.kommana@gmail.com>
2022-09-15 14:09:18 +0530

    Solved Objects.js

--- javascript_101/src/Objects.js
+++ javascript_101/src/Objects.js
@@ -10,9 +10,19 @@ OUTPUT: 	array containing the property names of obj.
 
 ERROR CASES: return null if obj is null or is not a Javascript object.
 */
-exports.GetObjectPropertyNames = function(obj) {
-
-
+exports.GetObjectPropertyNames = function (obj) {
+    if (obj instanceof Object) {
+        let result = []
+        for (let prop in obj) {
+            result.push(prop);
+        }
+        // Another way of iterating through the properties of the object.
+        // Object.keys(obj).forEach(key => {
+        //     result.push(key);
+        // });
+        return result;
+    }
+    return null;
 }
 
 /*
@@ -27,8 +37,14 @@ OUTPUT: 	array containing the property names of obj.
 
 ERROR CASES: return null if obj is null or is not a Javascript object.
 */
-exports.GetObjectPropertyValues = function(obj) {
-
-
+exports.GetObjectPropertyValues = function (obj) {
+    if (obj instanceof Object) {
+        let result = [];
+        for (let prop in obj) {
+            result.push(obj[prop]);
+        }
+        return result;
+    }
+    return null;
 }
 

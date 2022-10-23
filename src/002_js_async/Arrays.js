f325a74468ec6610620823f479ea55240a26a443
srikomm <sriharsha.kommana@gmail.com>
2022-10-23 22:17:40 +0530

    Update Arrays.js

--- src/002_js_async/Arrays.js
+++ src/002_js_async/Arrays.js
@@ -8,8 +8,48 @@ const Arrays = function () {
      Return the max element of array in the callback in error first way.
      */
     this.findMax = function (array, callback) {
+        if (array instanceof Array) {
 
+            if (array.length === 0) {
+                return callback(null, null);
+            }
+
+            let max = array[0];
+            array.forEach(i => {
+                if (i > max) {
+                    max = i;
+                }
+            });
+            return callback(null, max);
+        }
+        let err = {};
+        err.code = 100;
+        err.message = "Please give proper input";
+        return callback(err);
     };
+
+    this.fetchArraysFromFile = function () {
+        const path = require("path");
+        const fs = require("fs");
+        const fileName = path.join(__dirname, '..', '..', 'files') + '/arrays.txt';
+        let result = [];
+        let data = fs.readFileSync(fileName, 'utf8')
+        let lines = data.split('\n').slice(0, -1);
+        let lineArray = [];
+
+        lines.forEach(line => {
+            lineArray = [];
+            line = line.split(' ').slice(0, -1);
+            line.forEach(char => {
+                lineArray.push(parseInt(char));
+            });
+            if (lineArray !== []){
+                result.push(lineArray);
+            }
+        });
+        return result;
+    }
+
 
     /**
      Utility function used to find maximum element of given arrays
@@ -25,7 +65,17 @@ const Arrays = function () {
      Example input [[3, 6, 1], [5, 7, 8, 1], [5,8,3]] output [6, 8, 8]
      */
     this.getMaxArray = function (callback) {
-
+        let arrays = this.fetchArraysFromFile(callback);
+        let result = [];
+        arrays.forEach(array => {
+            this.findMax(array, function (er, res) {
+                if (er) {
+                    throw er;
+                }
+                result.push(res);
+            });
+        });
+        return callback(null, result);
     };
 };
 

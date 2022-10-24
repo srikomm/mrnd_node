f0db2143295e7e55088b12e6b893f6aa4661e806
srikomm <sriharsha.kommana@gmail.com>
2022-10-24 16:27:18 +0530

    Update Weather.test.js

--- spec/002_js_async/Weather.test.js
+++ spec/002_js_async/Weather.test.js
@@ -2,73 +2,64 @@ const request = require('request');
 
 const Weather = require("../../src/002_js_async/Weather.js");
 
-const getWeatherByCityName = function (cityName,callback) {
-    const queryParams = { q:cityName, APPID:'6a56e45751151c2322727cd275197b4e' };
+const getWeatherByCityName = function (cityName, callback) {
+    const queryParams = {q: cityName, APPID: '68410394bc6d84cbbac16d4419cf93a7'};
 
-    request({url:"https://api.openweathermap.org/data/2.5/weather", qs:queryParams}, function(err, response, body) {
-        callback(err,response,body);
+    request({url: "https://api.openweathermap.org/data/2.5/weather", qs: queryParams}, function (err, response, body) {
+        callback(err, response, body);
     });
 };
 
-describe("Weather module test suite", function() {
+describe("Weather module test suite", function () {
 
-    it("Should find temperature of a city by name", function(done) {
-
-        const self = this;
+    it("Should find temperature of a city by name", function (done) {
         let expectedTemperature = 0;
         try {
-            Weather.findCurrentTemperatureByCityName("Delhi,IN", function(err, temperature) {
+            Weather.findCurrentTemperatureByCityName("Delhi,IN", function (err, temperature) {
                 try {
                     expect(err).toBe(null);
 
-                    getWeatherByCityName("Delhi,IN",function(err,response,weather){
+                    getWeatherByCityName("Delhi,IN", function (err, response, weather) {
 
-                        if(!err){
+                        if (!err) {
                             weather = JSON.parse(weather);
 
-                            if(weather && weather["main"] && weather["main"]["temp"]){
+                            if (weather && weather["main"] && weather["main"]["temp"]) {
                                 expectedTemperature = weather.main.temp - 273.15;
-                            }
-                            else{
-                                self.fail(Error("test failed as it was not able to fetch the required temperature data to test"));
+                            } else {
+                                throw new Error("test failed as it was not able to fetch the required temperature data to test");
                             }
                             expect(temperature).toBe(expectedTemperature);
-                        }
-                        else{
-                            self.fail(Error("test failed as it was not able to fetch the required data to test"));
+                        } else {
+                            throw new Error("test failed as it was not able to fetch the required data to test");
                         }
 
                         done();
                     })
 
-                } catch(e) {
-                    self.fail(Error("test failed due to "+JSON.stringify(e)));
+                } catch (e) {
+                    throw new Error("test failed due to " + JSON.stringify(e));
                 }
             });
-        } catch(e) {
-            self.fail(Error("test failed due to "+JSON.stringify(e)));
-            done();
+        } catch (e) {
+            throw new Error("test failed due to " + JSON.stringify(e));
         }
     });
 
-    it("Should return appropriate error for invalid city name", function(done) {
-
-        const self = this;
-
+    it("Should return appropriate error for invalid city name", function (done) {
         try {
-            Weather.findCurrentTemperatureByCityName("", function(err, temperature) {
+            Weather.findCurrentTemperatureByCityName("", function (err, temperature) {
                 try {
                     expect(err).not.toBe(null);
                     expect(temperature).toBe(null);
                     expect(err.code).toBe("502");
-                } catch(e) {
-                    self.fail(Error("test failed due to "+JSON.stringify(e)));
+                } catch (e) {
+                    throw new Error("test failed due to " + JSON.stringify(e));
                 }
                 done();
             });
-        } catch(e) {
-            self.fail(Error("test failed due to "+JSON.stringify(e)));
-            done();
+        } catch (e) {
+            throw new Error("test failed due to " + JSON.stringify(e));
         }
     });
 
